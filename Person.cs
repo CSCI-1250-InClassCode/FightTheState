9b6e3e3bee9ba686ae7c867e9c1cd86fdfe9fc8f
desjardinsm <desjardinsm@MLC23000>
2024-11-20 16:40:32 -0500

    Add project files.

---
+++ Person.cs
@@ -0,0 +1,55 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace FightTheState
+{
+    public class Person
+    {
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+
+        public DateTime DOB { get; set; }
+
+        public bool Happy { get; private set; }
+
+
+
+        public Person(string firstName, string lastName)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            DOB = DateTime.Now;
+            Happy = true;
+
+        }
+
+
+        public string FullName()
+        {
+            return $"{FirstName} {LastName}";
+        }
+
+        public string ToDelimtedFileFormater(char delimiter = ',')
+        {
+            return $"{FirstName}{delimiter}{LastName}{delimiter}{DOB}{delimiter}{Happy}";
+        }
+
+        public static string CSVHeader(char delimeter = ',')
+        {
+            string header = string.Empty;
+
+            for (int i = 0; i < typeof(Person).GetProperties().Length; i++)
+            {
+                header += typeof(Person).GetProperties()[i].Name;
+                if(i != typeof(Person).GetProperties().Length - 1)
+                {
+                    header += delimeter;
+                }
+            }
+
+            return header ;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,88 @@
+﻿namespace FightTheState
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            int[] numbers = new int[32];
+
+            numbers[0] = 1;
+
+
+            for (int i = 0; i < numbers.Length; i++) 
+            {
+                numbers[i] = i;
+            }
+
+            int[] numbersnew = new int[33];
+
+            for (int i = 0;i < numbers.Length;i++)
+            {
+                numbersnew[i] = numbers[i];
+            }
+
+            //get the last one into numbersnew
+
+            numbersnew[numbersnew.Length - 1] = int.Parse(Console.ReadLine()); // new value you want to get int
+
+            List<int> listOfNumbers = new List<int>();
+
+            for (int i = 0;i < listOfNumbers.Count ; i++) 
+            {
+                listOfNumbers.Add(listOfNumbers[i]);
+            }
+
+            Console.WriteLine(listOfNumbers[listOfNumbers.Count - 1]);
+
+            listOfNumbers.Add(12);
+            */
+
+            KeyValuePair<int, string> pair = new KeyValuePair<int, string>(1, "one");
+
+           
+
+
+            Dictionary<int, string> mydictionary = new Dictionary<int, string>();
+
+
+
+            mydictionary.Add(1, "one");
+            mydictionary.Add(2, "two");
+            mydictionary.Add(3, "three");
+
+            Console.WriteLine(mydictionary[3] = "THREE!!!!!");
+
+            foreach (var item in mydictionary)
+            {
+                Console.WriteLine(item.Value);
+            }
+
+
+            Dictionary<string, Person> studentList = new Dictionary<string, Person>();
+
+            studentList.Add("E00000000", new Person("Mathew", "Desjardins"));
+            studentList.Add("E00000001", new Person("Mathew", "Desjardins"));
+            studentList.Add("E00000002", new Person("Mathew", "Desjardins"));
+            studentList.Add("E00000003", new Person("Mathew", "Desjardins"));
+            studentList.Add("E00000004", new Person("Mathew", "Desjardins"));
+            studentList.Add("E00000005", new Person("Mathew", "Desjardins"));
+            studentList.Add("E00000006", new Person("Mathew", "Desjardins"));
+
+
+            string output = Person.CSVHeader() + "\n";
+
+            foreach (var item in studentList)
+            {
+                output += item.Value.ToDelimtedFileFormater() + "\n";
+            }
+
+
+            //streamwriter output
+
+            Console.WriteLine(output);
+
+
+        }
+    }
+}
